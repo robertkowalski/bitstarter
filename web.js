5150b81b8a0af293b1b2534c5339b93888552442
Robert Kowalski <rok@kowalski.gd>
2013-07-05 17:49:30 +0200

    hw-3.1

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var buffer = fs.readFileSync('./index.html');
+
+app.get('/', function(req, res) {
+  res.send(buffer.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
